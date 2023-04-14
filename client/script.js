13564848dfdeac8411aa6e4ed8b2e668f3c923c9
MarcinBedorf <bedorf.marcin@gmail.com>
2023-04-14 22:25:40 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -105,7 +105,7 @@ const handleSubmit = async e => {
 	chatContainer.scrollTop = chatContainer.scrollHeight;
 	const messageDiv = document.getElementById(uniqueId);
 	loader(messageDiv);
-	const response = await fetch('http://localhost:5000', {
+	const response = await fetch('https://assistant-i7uf.onrender.com', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
