3a0ab761c050c6d263f113612c70371b5f69614a
MarcinBedorf <bedorf.marcin@gmail.com>
2023-04-15 20:26:34 +0200

    modified davinci model

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
 		const response = await openai.createCompletion({
 			model: 'text-davinci-003',
 			prompt: '',
-			temperature: 0.55,
+			temperature: 0.7,
 			max_tokens: 250,
 			top_p: 1,
 			frequency_penalty: 0.5,
