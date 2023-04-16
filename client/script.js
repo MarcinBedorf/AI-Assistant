2ed6ac494ef20022fde2ed872481bfc3650c4450
MarcinBedorf <bedorf.marcin@gmail.com>
2023-04-16 14:48:12 +0200

    server url and language model updated

--- client/script.js
+++ client/script.js
@@ -108,7 +108,7 @@ const handleSubmit = async e => {
 	const messageDiv = document.getElementById(uniqueId);
 	loader(messageDiv);
 
-	const response = await fetch('http://localhost:5000', {
+	const response = await fetch('https://assistant-1zh2.onrender.com', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
